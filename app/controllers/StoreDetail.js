f3641455b306db063ce6e88988ac8493733baf87
Jitendra Sharma <jitendra@atxlearning.com>
2017-08-14 20:35:23 +0530

    currented issue of next privous on store detail page

--- app/controllers/StoreDetail.js
+++ app/controllers/StoreDetail.js
@@ -13,6 +13,7 @@ define([
     function ($scope, $loadingOverlay, $modal, blinkaiService, localStorageService) {
       $scope.pageSize = 1;
       $scope.storedata = [];
+        $scope.currentPage=0;
         $scope.privateToken = localStorageService.get('privateToken');
         $loadingOverlay.show();
 
@@ -26,6 +27,13 @@ define([
         $scope.numberOfPages=function(){
              return Math.ceil($scope.conversations.entries.data.length/$scope.pageSize);
          }
+        $scope.clickPreviousStoreDetail=function(){
+          $scope.currentPage=$scope.currentPage-1;
+
+        }
+        $scope.clickNextStoreDetail=function(){
+          $scope.currentPage=$scope.currentPage+1;
+        }
     }
   ]);
 });
